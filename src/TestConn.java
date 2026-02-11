73a0b4f88cc7bd54ba930277cbd6522d1257e369
Daniel Fernandes <danieldfcreis@gmail.com>
2026-02-11 02:43:47 -0300

    aprendendo insersao

--- src/TestConn.java
+++ src/TestConn.java
@@ -1,11 +1,20 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class TestConn {
     public static void main(String[] args) throws Exception {
 
-      Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/customerdb","root","1234567");
+     try( Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/customerdb","root","1234567")){
         System.out.println("Connected to database successfully");
-        conn.close();
+
+        try(Statement stmt = conn.createStatement()){
+            stmt.executeUpdate("INSERT INTO customer(name,email,age) VALUES ('Daniel','d@d.com',22)");
+        }
+
+         System.out.println("Customer inserted successfully");
+
+     }
+
     }
 }
