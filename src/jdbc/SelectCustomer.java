653588c56476faebc5ef20629dd96c7575542914
Daniel Fernandes <danieldfcreis@gmail.com>
2026-02-12 23:16:26 -0300

    Trabalhando com meta dados

--- src/jdbc/SelectCustomer.java
+++ src/jdbc/SelectCustomer.java
@@ -1,4 +1,20 @@
 package jdbc;
 
+import java.sql.*;
+
 public class SelectCustomer {
+    public static void main(String[] args) throws Exception {
+       try(Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/customerdb","root","1234567");) {
+
+           System.out.println("success");
+
+           try(PreparedStatement stmt = conn.prepareStatement("select count(*) from customer")){
+               try(ResultSet rs = stmt.executeQuery()){
+                   rs.next();
+                  int count = rs.getInt(1);
+                   System.out.println("count= "+count);
+               }
+           }
+       }
+    }
 }
--- src/metadados/MetaData.java
+++ src/metadados/MetaData.java
@@ -1,4 +1,19 @@
 package metadados;
 
+import java.sql.*;
+
 public class MetaData {
+    public static void main(String[] args) throws SQLException {
+        try(Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/customerdb","root","1234567")){
+            System.out.println("Connected to database successfully");
+
+            DatabaseMetaData dbmd = conn.getMetaData();
+           try (ResultSet rs =  dbmd.getTables(null, null, null, new String[]{"TABLE"})) {
+               while (rs.next()) {
+                  String tableName = rs.getString("TABLE_NAME");
+                   System.out.println(tableName);
+               }
+           }
+        }
+    }
 }
